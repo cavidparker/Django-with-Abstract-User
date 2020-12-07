ec33a61e2639f187426757e2c1d20184d46e008a
Tariqul Islam <cavidpark@gmail.com>
2020-12-07 18:38:44 +0600

    full file

---
+++ movies/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import Movie, Film, Commercial
+from actors.models import Actor
+# Register your models here.
+
+
+class FilmModelAdmin(admin.ModelAdmin):
+    def render_change_form(self, request, context, *args, **kwargs):
+        context['adminform'].form.fields['actors'].queryset = Actor.objects.filter(is_star=True)
+        return super().render_change_form(request, context, *args, **kwargs)
+
+
+class CommercialModelAdmin(admin.ModelAdmin):
+    def render_change_form(self, request, context, *args, **kwargs):
+        context['adminform'].form.fields['actors'].queryset = Actor.objects.filter(is_star=False)
+        return super().render_change_form(request, context, *args, **kwargs)      
+
+admin.site.register(Film, FilmModelAdmin)
+admin.site.register(Commercial, CommercialModelAdmin)
+
+
